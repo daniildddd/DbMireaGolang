1277741e4fe5c0d541cb2dfb5dd69d70cf9cb9c6
Lyapunov Daniil <danyalyapunov@gmail.com>
2025-10-06 02:23:59 +0300

    feat: add metadata

---
+++ internal/ui/metadata/table_config.go
@@ -0,0 +1,33 @@
+package metadata
+
+// конфигурация таблиц и полей
+
+// в чем идея и почему вообще этот файл появился, мы тут будем настраивать форму, то есть описывать таблицы и ее поля(ограничения и так далее), это облегчит создания форм и их заполнение
+
+// тип для описания типов полей таблицы
+type fieldType string
+
+const (
+	FieldTypeText     fieldType = "text"
+	FieldTypeNumber   fieldType = "number"
+	FieldTypeDecimal  fieldType = "decimal"
+	FieldTypeDate     fieldType = "date"
+	FieldTypeDateTime fieldType = "datetime"
+)
+
+type FieldConfig struct {
+	Label       string             // подпись над полем ввода
+	Type        fieldType          // тип поля
+	Required    bool               //обязательное поле или нет
+	MaxLen      int                // максимальная длина
+	Placeholder string             // подпись в поле ввода
+	Options     []string           //для enum типа
+	Validator   func(string) error //валидация для этого поля
+}
+
+type TableConfig struct {
+	TableName   string                 // название таблицы(en)
+	DisplayName string                 // выводимое название таблицы(ru)
+	Fields      map[string]FieldConfig // конфиг полей
+	FieldsOrder []string               // порядок отображения
+}
