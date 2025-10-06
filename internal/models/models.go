a5ed5a9a93b657cba71c37c253b6c499e53e0930
Lyapunov Daniil <danyalyapunov@gmail.com>
2025-10-07 01:52:06 +0300

    feat: add struct models.go

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,3 +1,32 @@
 package models
 
-// здесь будут описаны модели таблицы для gorm
+// кастомный тип CaffeineLevel(enum)
+type CaffeineLevel string
+
+const (
+	Low        CaffeineLevel = "low"
+	Medium     CaffeineLevel = "medium"
+	High       CaffeineLevel = "high"
+	ExtraHight CaffeineLevel = "extra_high"
+)
+
+type Product struct {
+}
+
+type ProductionBatch struct{}
+
+type Inventory struct{}
+
+type Sale struct{}
+
+// функция возвращающая название таблицы
+func (Product) TableName() string { return "products" }
+
+// функция возвращающая название таблицы
+func (ProductionBatch) TableName() string { return "production_batches" }
+
+// функция возвращающая название таблицы
+func (Inventory) TableName() string { return "inventory" }
+
+// функция возвращающая название таблицы
+func (Sale) TableName() string { return "sale" }
