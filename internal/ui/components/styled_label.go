080ae1c544717ed824b9d3c980815c8e030f8fa8
Lyapunov Daniil <danyalyapunov@gmail.com>
2025-10-07 12:07:08 +0300

    feat: add styled_label.go

--- internal/ui/components/styled_label.go
+++ internal/ui/components/styled_label.go
@@ -1,3 +1,18 @@
 package components
 
-// здесь будут переиспользуемые компоненты
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/widget"
+)
+
+// функция для сощлдания и настройки виджета
+//
+// принимает текстовое содержимое метки(text), выравнивание текста(aligment) и флаг, указывающий должен ли текст быть жирным, возвращает указатель на созданный и настроенный виджет метки
+func CreateStyledLabel(text string, aligment fyne.TextAlign, bold bool) *widget.Label {
+	label := widget.NewLabel(text)
+	label.Alignment = aligment
+	if bold {
+		label.TextStyle = fyne.TextStyle{Bold: true}
+	}
+	return label
+}
