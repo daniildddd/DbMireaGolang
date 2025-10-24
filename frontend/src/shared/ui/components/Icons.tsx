69028105fe4ebe1b269fd0d0eb9c650d302927b5
Vladimir Valekzhanin <valdemarcheck@yandex.ru>
2025-10-24 18:50:45 +0300

    feat: icon components

---
+++ frontend/src/shared/ui/components/Icons.tsx
@@ -0,0 +1,24 @@
+import { Xmark } from "@gravity-ui/icons";
+import { Check } from "@gravity-ui/icons";
+import { AbbrSql } from "@gravity-ui/icons";
+import { ArrowsRotateRight } from "@gravity-ui/icons";
+import { ChevronsExpandUpRight } from "@gravity-ui/icons";
+import { ChevronsCollapseUpRight } from "@gravity-ui/icons";
+import clsx from "clsx";
+
+type IconComponent = typeof Xmark;
+
+function addClasses(IconComponent: IconComponent, className: string) {
+  return () => <IconComponent className={clsx("icon", className)} />;
+}
+
+const Icons: { [key: string]: IconComponent } = {
+  Delete: addClasses(Xmark, "delete-icon"),
+  Success: addClasses(Check, "success-icon"),
+  AbbrSql: addClasses(AbbrSql, "abbr-sql-icon"),
+  Refresh: addClasses(ArrowsRotateRight, "refresh-icon"),
+  Expand: addClasses(ChevronsExpandUpRight, "expand-icon"),
+  Shrink: addClasses(ChevronsCollapseUpRight, "shrink-icon"),
+};
+
+export default Icons;
