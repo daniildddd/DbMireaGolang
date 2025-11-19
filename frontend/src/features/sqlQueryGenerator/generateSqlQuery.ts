be250c1421b45f59a0179d477afee93198897583
vladcheck <valdemarcheck@yandex.ru>
2025-11-19 22:10:25 +0300

    feat: refactor generateSqlQuery file

--- frontend/src/features/sqlQueryGenerator/generateSqlQuery.ts
+++ frontend/src/features/sqlQueryGenerator/generateSqlQuery.ts
@@ -1,39 +1,78 @@
 import { Filters, FilterType } from "@/shared/types/filtering";
 
-function isFilterTypePresent(filters: Filters, type: FilterType) {
+type SelectPair = { column: string; as?: string };
+type Select = SelectPair[] | "*";
+
+function isFilterTypePresent(filters: Filters, type: FilterType): boolean {
   return filters[type].length > 0;
 }
 
-export function generateSqlQuery(
-  select: { column: string; as?: string }[] | "*" = "*",
-  table: string,
+function getSelectFromQuery(
+  select: Select = "*",
+  tableName: string,
   filters: Filters
-) {
+): string {
   let query =
     select === "*"
       ? "SELECT *"
-      : `${select
-          .map((pair) => `${pair.column}${pair.as ? ` AS ${pair.as}` : ""}`)
-          .join(", ")} `;
+      : `${select.map((pair) => `${pair.column}${pair?.as}`).join(", ")} `;
 
   if (isFilterTypePresent(filters, FilterType.aggregate)) {
     query += `, ${filters[FilterType.aggregate].join(", ")}`;
   }
 
-  query += ` FROM ${table}\n`;
+  query += ` FROM ${tableName}\n`;
+  return query;
+}
 
+function getWhereQuery(filters: Filters): string {
   if (isFilterTypePresent(filters, FilterType.where)) {
-    query += `\tWHERE ${filters[FilterType.where].join(" AND ")}\n`; // TODO: сделать умнее если нужно
+    return `\tWHERE ${filters[FilterType.where].join(" AND ")}\n`; // TODO: сделать умнее если нужно
+  } else {
+    return "";
   }
-  if (isFilterTypePresent(filters, FilterType.groupBy)) {
-    query += `\tGROUP BY ${filters[FilterType.groupBy].join(" AND ")}\n`; // TODO: сделать умнее если нужно
-  }
-  if (isFilterTypePresent(filters, FilterType.having)) {
-    query += `\tHAVING ${filters[FilterType.having].join(" AND ")}\n`; // TODO: сделать умнее если нужно
-  }
-  if (isFilterTypePresent(filters, FilterType.orderBy)) {
-    query += `\tORDER BY ${filters[FilterType.orderBy].join(", ")}`;
-  }
+}
 
-  return query.trim() + ";";
+function getGroupByQuery(filters: Filters): string {
+  if (isFilterTypePresent(filters, FilterType.groupBy)) {
+    return `\tGROUP BY ${filters[FilterType.groupBy].join(" AND ")}\n`; // TODO: сделать умнее если нужно
+  } else {
+    return "";
+  }
+}
+
+function getHavingQuery(filters: Filters): string {
+  if (isFilterTypePresent(filters, FilterType.having)) {
+    return `\tHAVING ${filters[FilterType.having].join(" AND ")}\n`; // TODO: сделать умнее если нужно
+  } else {
+    return "";
+  }
+}
+
+function getOrderByQuery(filters: Filters): string {
+  if (isFilterTypePresent(filters, FilterType.orderBy)) {
+    return `\tORDER BY ${filters[FilterType.orderBy].join(", ")}`;
+  } else {
+    return "";
+  }
+}
+
+export function generateSqlQuery(
+  select: Select = "*",
+  tableName: string,
+  filters: Filters
+) {
+  let query = getSelectFromQuery(select, tableName, filters);
+  console.log(query);
+  query += getWhereQuery(filters);
+  console.log(query);
+  query += getGroupByQuery(filters);
+  console.log(query);
+  query += getHavingQuery(filters);
+  console.log(query);
+  query += getOrderByQuery(filters);
+  console.log(query);
+  query = query.trim() + ";";
+
+  return query;
 }
